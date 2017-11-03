b7770dcdd6819fe9f0d14907f9b684c4f8cb5e2c
Tyler Cosgrove <cosgrove.tyler.98@gmail.com>
2017-11-03 15:00:46 -0400

    Manual refresh - Swipe down to refresh

--- app/src/main/java/io/cosgrove/nowplaying_history/SongHistoryActivity.java
+++ app/src/main/java/io/cosgrove/nowplaying_history/SongHistoryActivity.java
@@ -2,6 +2,7 @@ package io.cosgrove.nowplaying_history;
 
 import android.content.Intent;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,10 +15,11 @@ import io.cosgrove.nowplaying_history.models.SongHistory;
 import io.cosgrove.nowplaying_history.utils.Constants;
 import io.cosgrove.nowplaying_history.utils.SongHistoryStore;
 
-public class SongHistoryActivity extends AppCompatActivity {
+public class SongHistoryActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private SongHistoryAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,8 @@ public class SongHistoryActivity extends AppCompatActivity {
 
 
         mRecyclerView = (RecyclerView) findViewById(R.id.song_history_recycler_view);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
 
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
@@ -49,5 +53,13 @@ public class SongHistoryActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public void onRefresh() {
+        List<SongHistory> newSongHistoryList = SongHistoryStore.get(this).getSongHistory();
+        mAdapter.setDataset(newSongHistoryList);
+        mAdapter.notifyDataSetChanged();
+        mSwipeRefreshLayout.setRefreshing(false);
     }
 }
